fd90767d1b1e6479b98625536ae62e0fbc9532db
Martin Martincorena <martinmartincorena@gmail.com>
2023-07-11 19:24:39 -0300

    ➖ dependencies removed

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,64 +1,14 @@
-/* import { useEffect, useState } from "react";
-import HeroCard from "../components/HeroCard/HeroCard";
-import "./home.css";
-import Navbar from "../components/Navbar/Navbar";
-
-const Home = () => {
-  const urlAPI =
-    "https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=ba9f38df312b99cb16056792f9585fdb&hash=2c00c977d27cf34cddf75dc477d7f831";
-
-  const [characters, setCharacters] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(urlAPI);
-        const data = await response.json();
-
-        // Guardar los datos de los personajes en el estado
-        setCharacters(data.data.results);
-      } catch (error) {
-        console.log("No se pudo tener acceso a los datos de la API");
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  return (
-    <>
-      <Navbar />
-      <div className="card-container">
-        {characters.map((character) => (
-          <HeroCard
-            key={character.id}
-            name={character.name}
-            image={
-              character.thumbnail?.path +
-              "/standard_fantastic." +
-              character.thumbnail?.extension
-            }
-          />
-        ))}
-      </div>
-    </>
-  );
-};
-
-export default Home; */
-
-/* ----------------- */
-
 import { useEffect, useState } from "react";
 import HeroCard from "../components/HeroCard/HeroCard";
 import Navbar from "../components/Navbar/Navbar";
-import './Home.css'
+import "./Home.css";
 
 const Home = () => {
   const urlAPI =
     "https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=ba9f38df312b99cb16056792f9585fdb&hash=2c00c977d27cf34cddf75dc477d7f831";
 
   const [characters, setCharacters] = useState([]);
+
   const [filter, setFilter] = useState("");
 
   useEffect(() => {
@@ -86,10 +36,12 @@ const Home = () => {
       <Navbar onFilterChange={handleFilterChange} />
       <div className="card-container">
         {characters.map((character) => {
-          if (filter && !character.name.toLowerCase().includes(filter.toLowerCase())) {
+          if (
+            filter &&
+            !character.name.toLowerCase().includes(filter.toLowerCase())
+          ) {
             return null;
           }
-
           return (
             <HeroCard
               key={character.id}
@@ -108,4 +60,3 @@ const Home = () => {
 };
 
 export default Home;
-
